a963e5a34121d6fb932fd07967ee25939c2358c6
Ishan Parmar <ishanparmar@gmail.com>
2016-09-19 20:09:35 -0400

    created Java Module

---
+++ TrainingJava/src/org/training/useless/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.training.useless;
+
+/**
+ * Created by ishan on 9/19/2016.
+ */
+public class HelloWorld {
+    public static void main(String []args){
+        System.out.println("Hello World");
+    }
+}
